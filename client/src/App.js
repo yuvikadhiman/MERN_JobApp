b5488fc4ce8ca8d9d76e473490c4373452ff4686
yuvikadhiman <yuvikadhiman25@gmail.com>
2023-08-16 00:26:49 +0530

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,38 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {ToastContainer} from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
-import { Landing, Error, Register } from "./pages";
-import Dashboard from "./pages/dashboard/Dashboard";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Landing, Error, Register, ProtectedRoute } from './pages';
+
 import {
   Profile,
   AddJob,
   AllJobs,
   Stats,
   SharedLayout,
-} from './pages/dashboard';
+} from "./pages/dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/landing" element={<Landing />} />
-        <Route path="/register" element={<Register />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<AllJobs />} />
+          <Route path="stats" element={<Stats />} />
+          <Route path="add-job" element={<AddJob />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+        <Route path="landing" element={<Landing />} />
+        <Route path="register" element={<Register />} />
         <Route path="*" element={<Error />} />
       </Routes>
-      <ToastContainer position="top-center"/>
+      <ToastContainer position="top-center" />
     </BrowserRouter>
   );
 }
--- client/src/components/Logo.js
+++ client/src/components/Logo.js
@@ -1,4 +1,4 @@
-import logo from '../assets/images/logo.png';
+import logo from '../assets/images/logo.svg';
 
 const Logo = () => {
   return <img src={logo} alt='jobster logo' className='logo' />;
